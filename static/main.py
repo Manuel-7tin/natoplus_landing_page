5dbd205a672c5284cafa8837ed7edf050d3c7cf5
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-04-13 14:15:07 +0100

    Finalized page

--- static/main.py
+++ static/main.py
@@ -1,8 +1,10 @@
 # Import required libraries and packages
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_sqlalchemy import SQLAlchemy
+import socket
+import time
 
 import smtplib
 from email.message import EmailMessage
@@ -32,6 +34,7 @@ db.init_app(app)
 class Emails(db.Model):
     __tablename__ = "Email_addresses"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
     address: Mapped[str] = mapped_column(String, unique=True)
 
 
@@ -53,16 +56,33 @@ def send_welcome_mail(name, email, new=True):
     message["To"] = email
     message["Subject"] = "Newsletter signup confirmation"
     message.add_header("Reply-To", "noreply@natomail.com")
+
+    disclaimer = "This mail is sent from an unmonitored mailbox. Please do not reply."
+    welcome_msg = f"Welcome aboard {name}! 🎉 You're now part of our exclusive newsletter community.\n" \
+                  f"Get ready to unlock exciting updates, special offers, and insider insights delivered straight" \
+                  f"to your inbox.\nStay tuned for a journey filled with knowledge, inspiration, and surprises! 🚀" \
+                  f"\n\n\n{disclaimer}"
+    overdo_msg = f"Welcome back {name}! 🌟 It looks like you're already part of our amazing newsletter community.\n" \
+                 f"No need to sign up again – you're already set to receive our latest updates, exclusive offers, and" \
+                 f"insider content.\nSit back, relax, and enjoy the journey ahead! ✨" \
+                 f"\n\n\n{disclaimer}"
     if new:
-        message.set_content(f"Welcome dear {name}\n\nYou have succesfully signed up for our newsletter😊")
+        message.set_content(welcome_msg)
     else:
-        message.set_content(f"Welcome dear {name}\n\nYou were signed up for our newsletter before now😊")
+        message.set_content(overdo_msg)
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host="smtp.gmail.com", port=465, context=context) as connection:
-        connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
-        connection.sendmail(from_addr=MAIL_APP_PW, to_addrs=email, msg=message.as_string())
+    while True:
+        try:
+            with smtplib.SMTP_SSL(host="smtp.gmail.com", port=465, context=context) as connection:
+                connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
+                connection.sendmail(from_addr=MAIL_APP_PW, to_addrs=email, msg=message.as_string())
+        except socket.gaierror as e:
+            time.sleep(3)
+            print("there is an error:", e)
+        else:
+            break
 
 
 # Create flask routes
@@ -72,17 +92,27 @@ def home():
         read_data(email_list, Emails)
         data = request.form
         user_email = data["email"]
+        user_name = data["name"]
         email_saved = next((True for email in email_list if email.address == user_email), None)
         if not email_saved:
-            new_email = Emails(address=user_email)
+            new_email = Emails(name=user_name, address=user_email)
             with app.app_context():
                 db.session.add(new_email)
                 db.session.commit()
-            send_welcome_mail(name="customer", email=user_email)
+            send_welcome_mail(name=user_name, email=user_email)
         else:
-            send_welcome_mail(name="customer", email=user_email, new=False)
+            send_welcome_mail(name=user_name, email=user_email, new=False)
+        return render_template("done.html")
     return render_template("index.html")
 
+@app.route("/success")
+def go_back():
+    # time.sleep(3)
+    return redirect("https://natoplus.framer.ai")
+
+@app.route("/test")
+def temp():
+    return render_template("done.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
